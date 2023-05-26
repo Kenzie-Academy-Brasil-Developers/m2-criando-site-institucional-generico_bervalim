4c3dc06fadecbe0cd958be7fadb486a7a154a99e
bervalim <bernardogvalim@gmail.com>
2023-05-26 18:41:11 -0300

    Primeiro Envio Site Institucional

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,26 @@
-/* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+function handleModal() {
+  const registrationButtons = document.querySelectorAll(".button__container");
+  const modalController = document.querySelector(".modal__controller");
+  const modalInput = document.querySelector(".modal__input");
+
+  for (let i = 0; i < registrationButtons.length; i++) {
+    const registrationButton = registrationButtons[i];
+
+    registrationButton.addEventListener("click", function () {
+      modalController.showModal();
+      closeModal();
+      modalInput.value = "";
+    });
+  }
+}
+
+function closeModal() {
+  const closeModalButton = document.querySelector(".modal__close");
+  const modalController = document.querySelector(".modal__controller");
+
+  closeModalButton.addEventListener("click", function () {
+    modalController.close();
+  });
+}
+
+handleModal();
